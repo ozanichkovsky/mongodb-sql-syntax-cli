9ac029d68902725bd9ed0fbb0fd35ab7a83f0477
Olexandr Zanichkovsky <olexandr.zanichkovsky@zophiatech.com>
2016-08-05 14:20:05 +0300

    Fixed require_once paths

--- tests/Base.php
+++ tests/Base.php
@@ -7,9 +7,10 @@ use Silex\WebTestCase;
 class Base extends WebTestCase {
 
     public function createApplication() {
-        $app = require_once dirname(__DIR__) ."/../app/app.php";
-        require_once dirname(__DIR__) ."/../config/params.php";
-        require_once dirname(__DIR__) ."/../app/services.php";
+
+        $app = require_once  __DIR__ . "/../app/app.php";
+        require_once __DIR__ . "/../config/params.php";
+        require_once __DIR__ . "/../app/services.php";
 
         return $app;
     }
