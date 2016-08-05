71b1d5cfe6f85c383efcb4aa2eda43b9f23e86d7
Olexandr Zanichkovsky <olexandr.zanichkovsky@zophiatech.com>
2016-08-05 12:59:11 +0300

    Use CI to run tests on PHP 5.6 and PHP 7.0

---
+++ ci/docker_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# We need to install dependencies only for Docker
+[[ ! -e /.dockerenv ]] && [[ ! -e /.dockerinit ]] && exit 0
+
+set -xe
+
+# Install git (the php image doesn't have it) which is required by composer
+apt-get update -yqq
+apt-get install git -yqq
+
+# Install mongodb driver
+apt-get install libssl-dev -yqq
+pecl install mongodb && docker-php-ext-enable mongodb
+
+#install composer
+apt-get install curl -yqq && \
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
